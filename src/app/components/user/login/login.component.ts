ddaabc16483cb046a7766dbf6c8eab6e0c03b2f8
MANOJ PATRA <patra.manoj0@gmail.com>
2017-12-27 11:04:45 +0530

    Update login component

--- src/app/components/user/login/login.component.ts
+++ src/app/components/user/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../../logic/User';
+import { UserService } from '../../../services/user.service';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-login',
@@ -6,14 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  error: string = '';
+  private data: Observable<string>;
+  user: User;
 
-  constructor() { }
+  constructor(public auth: UserService) { }
 
   ngOnInit() {
+    this.user = new User();
   }
 
   logIn() {
+    this.error = '';
+    this.data = new Observable(observer => {
+      setTimeout(() => {
+          observer.next(this.auth.message);
+      }, 1000);
+    });
+
+    let subscription = this.data.subscribe(
+      value => this.error = value,
+      error => this.error = error
+    );
+
+    this.auth.login(this.user);
     
+    if(this.auth.isLoggedIn){
+      subscription.unsubscribe();
+    }
   }
 
 }
