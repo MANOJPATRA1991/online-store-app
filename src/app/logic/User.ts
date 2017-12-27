8a36d63517fe866e6b1ee4db65716b7216dae64c
MANOJ PATRA <patra.manoj0@gmail.com>
2017-12-27 11:04:31 +0530

    Add user logic

---
+++ src/app/logic/User.ts
@@ -0,0 +1,12 @@
+export class User {
+    username: string = '';
+    lastname: string = '';
+    firstname: string = '';
+    email: string = '';
+    password: string = '';
+    repassword: string = '';
+
+    constructor() {
+
+    }
+}
