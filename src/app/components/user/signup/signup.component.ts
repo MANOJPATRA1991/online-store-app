cc69dedda9c1ff6eb3cece780a9974fa40a74aac
MANOJ PATRA <patra.manoj0@gmail.com>
2017-12-27 11:03:21 +0530

    Add signup component

--- src/app/components/user/signup/signup.component.ts
+++ src/app/components/user/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../../logic/User';
+import { UserService } from '../../../services/user.service';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
+  error: string = '';
+  private data: Observable<string>;
+  user: User;
+  constructor(public auth: UserService) { }
 
   ngOnInit() {
+    this.user = new User();
   }
 
+  signup() {
+    this.error = '';
+    this.data = new Observable(observer => {
+      setTimeout(() => {
+          observer.next(this.auth.message);
+      }, 1000);
+    });
+
+    let subscription = this.data.subscribe(
+      value => this.error = value,
+      error => this.error = error
+    );
+
+    if (this.user.password === this.user.repassword) {
+      this.auth.register(this.user);
+    } else {
+      this.error = "Passwords don't match"
+    }
+
+    if(this.auth.isLoggedIn){
+      subscription.unsubscribe();
+    }
+  }
 }
