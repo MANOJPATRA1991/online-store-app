8fa13ff04da0453fa6e347cc9406c7a159d7c17b
MANOJ PATRA <patra.manoj0@gmail.com>
2017-12-27 11:03:04 +0530

    Add navigation component

---
+++ src/app/components/nav/nav.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+
+  constructor(public auth: UserService) { }
+
+  ngOnInit() {
+  }
+
+  logout() {
+    this.auth.logout();
+  }
+
+}
