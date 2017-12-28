6aa48c30ef926b61009ac8ac77b7857a17648aa1
MANOJ PATRA <patra.manoj0@gmail.com>
2017-12-28 17:05:43 +0530

    Add functionality to add new group

--- src/app/services/group.service.ts
+++ src/app/services/group.service.ts
@@ -8,12 +8,14 @@ import 'rxjs/add/operator/catch';
 import { Subject } from 'rxjs/Subject';
 
 import { UserService } from './user.service';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 const baseURL = "http://localhost:3000";
 
 @Injectable()
 export class GroupService {
-
+  groups: BehaviorSubject<any> = new BehaviorSubject<any>([]);
+  
   constructor(public http: Http, public auth: UserService) { }
 
   getAllGroups() {
@@ -21,13 +23,20 @@ export class GroupService {
     .map(response => {
       console.log(response);
       return response.json();
-    })
+    });
   }
 
   updateItemGroup(itemId, newGroup) {
     return this.http.put(`${baseURL}/items/${itemId}/updateGroup`, newGroup, {headers: this.auth.headers})
     .map(response => {
       return response.json();
+    });
+  }
+  
+  addNewGroup(newGroup) {
+    return this.http.post(`${baseURL}/groups/newGroup`, newGroup, {headers: this.auth.headers})
+    .map(response => {
+      return response.json().groups;
     })
   }
 
