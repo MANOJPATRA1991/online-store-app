bbdb0c0204c200fbacc1ccd5716104ebe945e650
MANOJ PATRA <patra.manoj0@gmail.com>
2017-12-28 17:06:56 +0530

    Update module dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,42 +3,119 @@ import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { SignupComponent } from './components/user/signup/signup.component';
 import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import {
   MatCardModule,
   MatFormFieldModule,
   MatInputModule,
   MatToolbarModule,
   MatButtonModule,
-  MatGridListModule
+  MatGridListModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatMenuModule,
+  MatDialogModule,
+  MatSnackBarModule,
+  MatDatepickerModule,
+  MatSliderModule,
+  MatSelectModule,
+  MatNativeDateModule,
+  MatTabsModule,
+  MatProgressSpinnerModule,
+  MatProgressBarModule,
+  MatCheckboxModule
 } from '@angular/material';
+
+
+import { LayoutModule } from '@angular/cdk/layout';
+
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { FileSelectDirective } from 'ng2-file-upload';
 
 import { FormsModule } from '@angular/forms';
 
 import { routing } from './routing';
+
+import { SignupComponent } from './components/user/signup/signup.component';
 import { LoginComponent } from './components/user/login/login.component';
+import { NavComponent } from './components/nav/nav.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { UserService } from './services/user.service';
+import { UserComponent } from './components/user/user.component';
+import { EditUserComponent } from './components/user/edit-user/edit-user.component';
+import { CreateItemComponent } from './components/create-item/create-item.component';
+import { ItemService } from './services/item.service';
+import { EditItemComponent } from './components/edit-item/edit-item.component';
+import { GroupService } from './services/group.service';
+import { SearchPipe } from './pipes/search.pipe';
+import { ItemsListComponent } from './components/items-list/items-list.component';
+import { ItemDetailComponent } from './components/item-detail/item-detail.component';
+import { AddGroupComponent } from './components/add-group/add-group.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     SignupComponent,
-    LoginComponent
+    LoginComponent,
+    NavComponent,
+    DashboardComponent,
+    UserComponent,
+    EditUserComponent,
+    CreateItemComponent,
+    FileSelectDirective,
+    EditItemComponent,
+    SearchPipe,
+    ItemsListComponent,
+    ItemDetailComponent,
+    AddGroupComponent
+  ],
+  entryComponents: [
+    EditUserComponent,
+    CreateItemComponent,
+    EditItemComponent,
+    AddGroupComponent
   ],
   imports: [
     routing,
     BrowserModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
     FormsModule,
     RouterModule,
+    HttpModule,
     MatCardModule,
     MatFormFieldModule,
     MatInputModule,
     MatToolbarModule,
     MatButtonModule,
-    MatGridListModule
+    MatGridListModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatMenuModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    MatDatepickerModule,
+    MatSliderModule,
+    MatSelectModule,
+    MatNativeDateModule,
+    MatProgressSpinnerModule,
+    MatProgressBarModule,
+    MatTabsModule,
+    MatCheckboxModule,
+    LayoutModule
   ],
-  providers: [],
+  providers: [
+    UserService,
+    ItemService,
+    GroupService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
